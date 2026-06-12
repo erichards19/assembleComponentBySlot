02ea3060aeb0bf0452507fa30d2ab1884e8105ea
AKSJDHKJh234234 <55686347+AKSJDHKJh234234@users.noreply.github.com>
2026-06-11 23:26:46 -0400

    Update FindEoS.py

--- FindEoS.py
+++ FindEoS.py
@@ -5,7 +5,6 @@ from itkdb.core import User
 from pathlib import Path
 from getpass import getpass
 
-runNumFolder = Path("/home/stavetesting/Desktop/itsdaq-sw/DAT/etc")
 logpath = Path("/home/stavetesting/Desktop/itsdaq-sw/DAT/logs")
 
 def getToken():
@@ -22,45 +21,26 @@ def getToken():
             print("Authentication failed.")
     return client
 
-def findLastFile(logpath, name_pattern):
-    latest_file = None
-    latest_mtime = 0
+def lpgbtReport(runNum, folder):
+    for file in folder.glob("*.json"):
+        if "lpgbt_report_" + str(runNum) in file.name:
+            with open(file, "r") as f:
+                return json.load(f)
 
-    print("Scanning logs...")
-
-    # os.scandir loops through files efficiently without loading the whole list into memory
-    with os.scandir(logpath) as logs:
-        for files in logs:
-            # Check if it's a file and contains your specific number/format string
-            if files.is_file() and name_pattern in files.name:
-                # entry.stat() retrieves the modification time instantly
-                mtime = files.stat().st_mtime
-                if mtime > latest_mtime:
-                    latest_mtime = mtime
-                    latest_file = files.path
-
-    if not latest_file:
-        print(f"No files found matching the pattern: '{name_pattern}'")
-        return
-
-    print(f"Success! Found latest file: {latest_file}")
-    print(f"Extracting data starting from keyword: '{keyword}'\n")
-
-    return latest_file
 
 client = getToken()
-
-with open(findLastFile(logpath, "lpgbt_report"), "r") as f:
-    lpgbtReport = json.load(f)
-
+runNum = input("Enter Test Run Number: (Press RETURN if using current test)")
 eosSN=[None]*2
+EoS = lpgbtReport(runNum, logpath)
 for i in range(2):
-    eosSN[i] = (client.get("getComponent", json={
-    "component": lpgbtReport["port_"+str(i*2)]["eos_id"],
-    "alternativeIdentifier": True,})["serialNumber"])
+    eosSN[i] = client.get("getComponent", json={
+            "component": EoS["port_"+str(i*2)]["eos_id"],
+            "alternativeIdentifier": True,})["serialNumber"]
+    assembleEoS = client.post("assembleComponent", json={
+        "parent": parent,
+        "child": eosSN[i],
+        "properties": 
 
-print("MAIM side EoS SN: "+ str(eosSN[0]))
-print("SECONDARY side EoS SN: "+ str(eosSN[1]))
 
 
 
